5a4a44922cfd4e0f168e767e53115ebba3414a08
Webmaster <faraon9999faraon@gmail.com>
2026-05-05 17:24:50 +0600

    first commit

---
+++ main/static/main/js/javascript.js
@@ -0,0 +1,6 @@
+<script>
+function toggleSidebar() {
+    document.getElementById("sidebar").classList.toggle("active");
+    document.querySelector("main").classList.toggle("active");
+}
+</script>
