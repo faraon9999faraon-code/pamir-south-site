a017e34b71a8d2833dc3a18922118dff3059cca1
Webmaster <faraon9999faraon@gmail.com>
2026-05-25 22:56:12 +0600

    fix settings

--- itproger/settings.py
+++ itproger/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s#96l8groqd72%qcui97m&+m!8+1z4i7r5d!ec#%=45qoqz#y1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',]
 
 
 
